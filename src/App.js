b91112b5df7cb9f6e691ca5fd5bcd4dd1f779ff2
elamahpla <theamazingness@gmail.com>
2018-06-29 00:42:33 -0500

    Theme Switcher I might use in next project. Just wanted to have it around

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ThemeSwitcher from './ThemeSwitcher';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+        <ThemeSwitcher />
+      </div>
+    );
+  }
+}
+
+export default App;
